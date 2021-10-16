c506fed55cf9f3b8b5376a885c46db9fc95c64fe
nadiiazimina <84657846+nadiiazimina@users.noreply.github.com>
2021-10-16 14:02:25 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+function logNumbers(start, end) {
+  console.log(`iteratively looping from ${start} until{end}`);
+
+  for (var i = start; i <= end; i++) {
+    console.log("hitting index", i);
+  }
+}
+
+console.log("~~~ logNumbers ~~~");
+logNumbers(1, 2);
+
+function logNumbersRecursively(start, end) {
+  console.log(`recursively looping from ${start} until ${end}`);
+
+  function recurse(i) {
+    console.log("hitting index", i);
+
+    if (i < end) {
+      recurse(i + 1);
+    }
+  }
+  recurse(start);
+}
+console.log("~~~");
+logNumbersRecursively(1, 3);
